95852ced4ad56748c6612c30716b63e8b0fe5f7d
RobiulHossain180 <77151045+RobiulHossain180@users.noreply.github.com>
2023-10-01 20:51:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const fullscreenButton = document.getElementById("fullscreenButton");
+        const fullscreenContainer = document.getElementById("fullscreenContainer");
+        const searchInput = document.getElementById("searchInput");
+        const table = document.querySelector("table");
+        const rows = table.getElementsByTagName("tr");
+     // JavaScript for showing/hiding the scroll-to-top button
+     window.addEventListener("scroll", function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("scrollToTopButton").style.display = "block";
+        } else {
+            document.getElementById("scrollToTopButton").style.display = "none";
+        }
+    });
+
+    // JavaScript for scrolling to the top of the page
+    document.getElementById("scrollToTopButton").addEventListener("click", function() {
+        scrollToTop();
+    });
+
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+    searchInput.addEventListener("input", function() {
+            const searchTerm = searchInput.value.toLowerCase();
+
+            for (let i = 1; i < rows.length; i++) {
+                const cells = rows[i].getElementsByTagName("td");
+                const match = matchRow(cells, searchTerm);
+
+                if (match) {
+                    rows[i].style.display = "";
+                } else {
+                    rows[i].style.display = "none";
+                }
+            }
+        });
+
+        function matchRow(cells, searchTerm) {
+            for (let j = 1; j <= 2; j++) {
+                const cellContent = cells[j].textContent.toLowerCase();
+
+                if (cellContent.includes(searchTerm)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        // Function to request full screen
+        function requestFullScreen() {
+            if (fullscreenContainer.requestFullscreen) {
+                fullscreenContainer.requestFullscreen();
+            } else if (fullscreenContainer.mozRequestFullScreen) {
+                fullscreenContainer.mozRequestFullScreen();
+            } else if (fullscreenContainer.webkitRequestFullscreen) {
+                fullscreenContainer.webkitRequestFullscreen();
+            } else if (fullscreenContainer.msRequestFullscreen) {
+                fullscreenContainer.msRequestFullscreen();
+            }
+        }
+        fullscreenButton.addEventListener("click", () => {
+            requestFullScreen();
+        });

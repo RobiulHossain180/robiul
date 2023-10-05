3f09bb97963d265966accb94046b838a55e02283
RobiulHossain180 <77151045+RobiulHossain180@users.noreply.github.com>
2023-10-05 23:53:45 +0600

    Update

--- script.js
+++ script.js
@@ -65,3 +65,13 @@ const rows = table.getElementsByTagName("tr");
         fullscreenButton.addEventListener("click", () => {
             requestFullScreen();
         });
+        
+
+const countElement = document.getElementById('count');
+update();
+function update(){
+     fetch('https://api.countapi.xyz/hit/https://robiulhossain180.github.io/robiul/?amount=1')
+     .then(res=> res.json()).then(res => {
+        countElement.innerHTML = res.value;
+     })
+}
